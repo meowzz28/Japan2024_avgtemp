9220809a9ecf6e9c220674e01ba309f9f2344819
meowzz28 <165900800+meowzz28@users.noreply.github.com>
2024-07-23 03:53:54 +0800

    Update weather_plot.py

--- weather_plot.py
+++ weather_plot.py
@@ -10,28 +10,21 @@ reader = csv.reader(lines)
 header_row = next(reader)
 
 
-dates, highs, lows, avgs = [], [], [], []
+dates, avgs = [], []
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
     try:
         avg = int(row[3])
-        '''high = int(row[4])
-        low = int(row[5])'''
     except ValueError:
         print("Missing_Data")
     else:
         dates.append(current_date)
-        #highs.append(high)
-        #lows.append(low)
         avgs.append(avg)
 
 
 plt.style.use('bmh')
 fig, ax = plt.subplots()
-#ax.plot(dates, highs, color='red', alpha=0.5)
-#ax.plot(dates, lows, color='blue', alpha=0.5)
 ax.plot(dates, avgs, color='black', alpha=0.5)
-#ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 ax.set_title("Daily Avg Temperatures of Japan, 2024", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
